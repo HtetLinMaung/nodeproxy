e9287c8a3f0506fc2c714773e87a928450973608
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2022-01-21 14:57:39 +0630

    update

---
+++ app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const PORT = process.env.PORT || 7070;
+
+const app = express();
+
+app.use(
+  "/startup",
+  createProxyMiddleware({
+    target: "http://startup",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/juice",
+  createProxyMiddleware({
+    target: "http://juice",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/juiceweb",
+  createProxyMiddleware({
+    target: "http://juiceweb",
+    changeOrigin: true,
+  })
+);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(
+    `<div style="height: 100vh; display: flex; justify-content: center; align-items: center;">
+    <h1>Node Proxy Server Online</h1>
+    </div>`
+  );
+});
+
+app.listen(PORT, () => console.log(`Proxy Server listening on PORT ${PORT}`));
